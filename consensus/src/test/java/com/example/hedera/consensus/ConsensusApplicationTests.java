7a4acd72dc35545e045b19f2b9794f3570f0646e
JungHyeonKim <dev.kjung@gmail.com>
2024-12-29 09:18:47 +0900

    [Feat]: consensus module 생성

---
+++ consensus/src/test/java/com/example/hedera/consensus/ConsensusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hedera.consensus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsensusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
