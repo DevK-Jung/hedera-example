ab6ce5cd32617c40ee4f2c32f84e58ee3cbfd556
김정현 <wjdgus0117@ccmedia.co.kr>
2025-03-21 17:00:44 +0900

    [Feat]: 문서화 주석 위치 변경

--- consensus/src/main/java/com/example/hedera/consensus/helper/ConsensusHelper.java
+++ consensus/src/main/java/com/example/hedera/consensus/helper/ConsensusHelper.java
@@ -11,8 +11,34 @@ import java.time.Instant;
 import java.util.concurrent.TimeoutException;
 
 public interface ConsensusHelper {
+    /**
+     * 기본 설정을 사용해 새로운 토픽을 생성합니다.
+     * <p>
+     * 기본 설정:
+     * - AdminKey와 SubmitKey는 운영자의 개인 키를 사용합니다.
+     * - 자동 갱신 기간(Auto-renew period)은 92일로 설정됩니다.
+     *
+     * @param topicMemo 메모
+     * @return 생성된 토픽의 ID를 문자열로 반환합니다.
+     * @throws PrecheckStatusException 요청이 Hedera 네트워크의 사전 검사 단계에서 실패할 경우 발생합니다.
+     * @throws TimeoutException        요청 시간이 초과될 경우 발생합니다.
+     * @throws ReceiptStatusException  트랜잭션 영수증 상태가 실패로 반환될 경우 발생합니다.
+     */
     HederaTransactionResponseVo<TopicResponseVo> createTopic(String topicMemo) throws PrecheckStatusException, TimeoutException, ReceiptStatusException;
 
+    /**
+     * 사용자 정의 설정을 사용해 새로운 토픽을 생성합니다.
+     *
+     * @param adminKey           AdminKey는 토픽 업데이트 및 삭제를 제어합니다. (null로 설정하면 업데이트는 가능하지만 삭제는 불가능합니다.)
+     * @param submitKey          SubmitKey는 메시지 제출 권한을 제어합니다. (null로 설정하면 모든 사용자가 메시지를 제출할 수 있습니다.)
+     * @param topicMemo          토픽 메모(선택 사항, 최대 100바이트).
+     * @param autoRenewAccountId 자동 갱신 비용을 부담할 계정 ID(선택 사항).
+     * @param autoRenewPeriod    자동 갱신 주기(선택 사항, 30~92일 사이의 값).
+     * @return 생성된 토픽의 ID
+     * @throws PrecheckStatusException 요청이 Hedera 네트워크의 사전 검사 단계에서 실패할 경우 발생합니다.
+     * @throws TimeoutException        요청 시간이 초과될 경우 발생합니다.
+     * @throws ReceiptStatusException  트랜잭션 영수증 상태가 실패로 반환될 경우 발생합니다.
+     */
     HederaTransactionResponseVo<TopicResponseVo> createTopic(Key adminKey,
                                                              Key submitKey,
                                                              String topicMemo,
@@ -20,62 +46,210 @@ public interface ConsensusHelper {
                                                              Duration autoRenewPeriod)
             throws PrecheckStatusException, TimeoutException, ReceiptStatusException;
 
+    /**
+     * update adminKey
+     * 토픽 업데이트 및 토픽 삭제 거래를 승인하는 새로운 관리자 키를 설정합니다.
+     *
+     * @param topicId     topicId
+     * @param adminKey    거래 서명할 adminKey
+     * @param newAdminKey 수정할 adminKey
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> updateAdminKey(@NonNull String topicId,
                                                                 @NonNull String adminKey,
                                                                 @NonNull String newAdminKey)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * update submitKey
+     * 이 토픽에 메시지를 보내는 것을 허용하는 새로운 submitKey 를 토픽에 설정합니다.
+     *
+     * @param topicId      topicId
+     * @param adminKey     거래 서명할 adminKey
+     * @param newSubmitKey 수정할 submitKey
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> updateSubmitKey(@NonNull String topicId,
                                                                  @NonNull String adminKey,
                                                                  @NonNull String newSubmitKey)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * update expirationTime
+     * 만료 기간 수정
+     *
+     * @param topicId           topicId
+     * @param adminKey          거래 서명할 adminKey
+     * @param newExpirationTime 수정할 만료 기간
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> updateExpirationTime(@NonNull String topicId,
                                                                       @NonNull String adminKey,
                                                                       @NonNull Instant newExpirationTime)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * update expirationTime
+     * 만료 기간 수정
+     *
+     * @param topicId      topicId
+     * @param adminKey     거래 서명할 adminKey
+     * @param newTopicMemo 수정할 토픽 메모
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> updateTopicMemo(@NonNull String topicId,
                                                                  @NonNull String adminKey,
                                                                  @NonNull String newTopicMemo)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * update AutoRenewAccountId
+     * 자동 갱신 account 수정
+     *
+     * @param topicId               topicId
+     * @param adminKey              거래 서명할 adminKey
+     * @param newAutoRenewAccountId 수정할 자동 갱신 accountId
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> updateAutoRenewAccount(@NonNull String topicId,
                                                                         @NonNull String adminKey,
                                                                         @NonNull String newAutoRenewAccountId)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * update AutoRenewPeriod
+     * 자동 갱신 기간 수정
+     *
+     * @param topicId            topicId
+     * @param adminKey           거래 서명할 adminKey
+     * @param newAutoRenewPeriod 수정할 자동 갱신 기간
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> updateAutoRenewAccount(@NonNull String topicId,
                                                                         @NonNull String adminKey,
                                                                         @NonNull Duration newAutoRenewPeriod)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * clear adminKey
+     *
+     * @param topicId  topicId
+     * @param adminKey 거래 서명할 adminKey
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> clearAdminKey(@NonNull String topicId,
                                                                @NonNull String adminKey)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * clearSubmitKey
+     *
+     * @param topicId  topicId
+     * @param adminKey 거래 서명할 adminKey
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> clearSubmitKey(@NonNull String topicId,
                                                                 @NonNull String adminKey)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * clearTopicMemo
+     *
+     * @param topicId  topicId
+     * @param adminKey 거래 서명할 adminKey
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> clearTopicMemo(@NonNull String topicId,
                                                                 @NonNull String adminKey)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * clearAutoRenewAccountId
+     *
+     * @param topicId  topicId
+     * @param adminKey 거래 서명할 adminKey
+     * @return HederaTransactionResponseVo<TopicResponseVo>
+     * @throws ReceiptStatusException  ReceiptStatusException
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> clearAutoRenewAccountId(@NonNull String topicId,
                                                                          @NonNull String adminKey)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * delete Topic
+     * <p>토픽 삭제, 삭제 후 메시지 수신 불가하며 submitMessage 호출 실패, 토픽이 삭제된 후에도 미러 노드를 통해 이전 메시지 엑세스는 가능</p>
+     * <ul>
+     *      <li>토픽 생성 시 adminKey가 설정된 경우 토픽을 성공적으로 삭제하려면 adminKey에 서명해야 합니다.</li>
+     *      <li>토픽 생성 시 adminKey가 설정되지 않은 경우 토픽을 삭제할 수 없으며 UNAUTHHORIZED 오류가 발생합니다.</li>
+     * </ul>
+     *
+     * @param topicId  topicId
+     * @param adminKey 토픽 생성 시 설정한 adminKey
+     * @return
+     * @throws ReceiptStatusException
+     * @throws PrecheckStatusException
+     * @throws TimeoutException
+     */
     HederaTransactionResponseVo<TopicResponseVo> deleteTopic(@NonNull String topicId,
                                                              @NonNull String adminKey)
             throws ReceiptStatusException, PrecheckStatusException, TimeoutException;
 
+    /**
+     * topic 정보 조회.
+     *
+     * @param topicId 토픽 아이디
+     * @return TopicInfo
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     */
     TopicInfo getTopicInfo(String topicId) throws PrecheckStatusException, TimeoutException;
 
     void getTopicMessages(String topicId,
                           Instant subscribeStartTime,
                           Instant subscribeEndTime);
 
+    /**
+     * submit Message.
+     * <p>
+     *
+     * @param topicId    topicId
+     * @param message    message - 최대 크기 1024byte(1kb)
+     * @param chunkSize  메시지에 대한 개별 청크의 최대 크기 - default 1024
+     * @param maxChuncks 메시지 분할 할 수 있는 최대 청크 수 - 기본 값 20
+     * @return HederaTransactionResponseVo<MessageResponseVo>
+     * @throws PrecheckStatusException PrecheckStatusException
+     * @throws TimeoutException        TimeoutException
+     * @throws ReceiptStatusException  ReceiptStatusException
+     */
     HederaTransactionResponseVo<MessageResponseVo> submitMessage(@NonNull String topicId,
                                                                  @NonNull String message,
                                                                  Integer chunkSize,
--- consensus/src/main/java/com/example/hedera/consensus/helper/ConsensusHelperV1.java
+++ consensus/src/main/java/com/example/hedera/consensus/helper/ConsensusHelperV1.java
@@ -32,38 +32,13 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         this.privateKey = PrivateKey.fromString(privateKey);
     }
 
-    /**
-     * 기본 설정을 사용해 새로운 토픽을 생성합니다.
-     * <p>
-     * 기본 설정:
-     * - AdminKey와 SubmitKey는 운영자의 개인 키를 사용합니다.
-     * - 자동 갱신 기간(Auto-renew period)은 92일로 설정됩니다.
-     *
-     * @param topicMemo 메모
-     * @return 생성된 토픽의 ID를 문자열로 반환합니다.
-     * @throws PrecheckStatusException 요청이 Hedera 네트워크의 사전 검사 단계에서 실패할 경우 발생합니다.
-     * @throws TimeoutException        요청 시간이 초과될 경우 발생합니다.
-     * @throws ReceiptStatusException  트랜잭션 영수증 상태가 실패로 반환될 경우 발생합니다.
-     */
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> createTopic(String topicMemo) throws PrecheckStatusException, TimeoutException, ReceiptStatusException {
 
         return createTopic(privateKey, privateKey, topicMemo, accountId, Duration.ofDays(92));
     }
 
-    /**
-     * 사용자 정의 설정을 사용해 새로운 토픽을 생성합니다.
-     *
-     * @param adminKey           AdminKey는 토픽 업데이트 및 삭제를 제어합니다. (null로 설정하면 업데이트는 가능하지만 삭제는 불가능합니다.)
-     * @param submitKey          SubmitKey는 메시지 제출 권한을 제어합니다. (null로 설정하면 모든 사용자가 메시지를 제출할 수 있습니다.)
-     * @param topicMemo          토픽 메모(선택 사항, 최대 100바이트).
-     * @param autoRenewAccountId 자동 갱신 비용을 부담할 계정 ID(선택 사항).
-     * @param autoRenewPeriod    자동 갱신 주기(선택 사항, 30~92일 사이의 값).
-     * @return 생성된 토픽의 ID
-     * @throws PrecheckStatusException 요청이 Hedera 네트워크의 사전 검사 단계에서 실패할 경우 발생합니다.
-     * @throws TimeoutException        요청 시간이 초과될 경우 발생합니다.
-     * @throws ReceiptStatusException  트랜잭션 영수증 상태가 실패로 반환될 경우 발생합니다.
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> createTopic(Key adminKey,
                                                                     Key submitKey,
@@ -95,18 +70,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return makeTransactionResponse(receipt, new TopicResponseVo(newTopicId.toString()));
     }
 
-    /**
-     * update adminKey
-     * 토픽 업데이트 및 토픽 삭제 거래를 승인하는 새로운 관리자 키를 설정합니다.
-     *
-     * @param topicId     topicId
-     * @param adminKey    거래 서명할 adminKey
-     * @param newAdminKey 수정할 adminKey
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> updateAdminKey(@NonNull String topicId,
                                                                        @NonNull String adminKey,
@@ -119,18 +83,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return updateTopic(transaction, adminKey);
     }
 
-    /**
-     * update submitKey
-     * 이 토픽에 메시지를 보내는 것을 허용하는 새로운 submitKey 를 토픽에 설정합니다.
-     *
-     * @param topicId      topicId
-     * @param adminKey     거래 서명할 adminKey
-     * @param newSubmitKey 수정할 submitKey
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> updateSubmitKey(@NonNull String topicId,
                                                                         @NonNull String adminKey,
@@ -142,18 +95,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return updateTopic(transaction, adminKey);
     }
 
-    /**
-     * update expirationTime
-     * 만료 기간 수정
-     *
-     * @param topicId           topicId
-     * @param adminKey          거래 서명할 adminKey
-     * @param newExpirationTime 수정할 만료 기간
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> updateExpirationTime(@NonNull String topicId,
                                                                              @NonNull String adminKey,
@@ -166,18 +108,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return updateTopic(transaction, adminKey);
     }
 
-    /**
-     * update expirationTime
-     * 만료 기간 수정
-     *
-     * @param topicId      topicId
-     * @param adminKey     거래 서명할 adminKey
-     * @param newTopicMemo 수정할 토픽 메모
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> updateTopicMemo(@NonNull String topicId,
                                                                         @NonNull String adminKey,
@@ -190,18 +121,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return updateTopic(transaction, adminKey);
     }
 
-    /**
-     * update AutoRenewAccountId
-     * 자동 갱신 account 수정
-     *
-     * @param topicId               topicId
-     * @param adminKey              거래 서명할 adminKey
-     * @param newAutoRenewAccountId 수정할 자동 갱신 accountId
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> updateAutoRenewAccount(@NonNull String topicId,
                                                                                @NonNull String adminKey,
@@ -215,18 +135,6 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
     }
 
 
-    /**
-     * update AutoRenewPeriod
-     * 자동 갱신 기간 수정
-     *
-     * @param topicId            topicId
-     * @param adminKey           거래 서명할 adminKey
-     * @param newAutoRenewPeriod 수정할 자동 갱신 기간
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> updateAutoRenewAccount(@NonNull String topicId,
                                                                                @NonNull String adminKey,
@@ -239,16 +147,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return updateTopic(transaction, adminKey);
     }
 
-    /**
-     * clear adminKey
-     *
-     * @param topicId  topicId
-     * @param adminKey 거래 서명할 adminKey
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> clearAdminKey(@NonNull String topicId,
                                                                       @NonNull String adminKey)
@@ -260,16 +159,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return updateTopic(transaction, adminKey);
     }
 
-    /**
-     * clearSubmitKey
-     *
-     * @param topicId  topicId
-     * @param adminKey 거래 서명할 adminKey
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> clearSubmitKey(@NonNull String topicId,
                                                                        @NonNull String adminKey)
@@ -282,16 +172,6 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
     }
 
 
-    /**
-     * clearTopicMemo
-     *
-     * @param topicId  topicId
-     * @param adminKey 거래 서명할 adminKey
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> clearTopicMemo(@NonNull String topicId,
                                                                        @NonNull String adminKey)
@@ -303,16 +183,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return updateTopic(transaction, adminKey);
     }
 
-    /**
-     * clearAutoRenewAccountId
-     *
-     * @param topicId  topicId
-     * @param adminKey 거래 서명할 adminKey
-     * @return HederaTransactionResponseVo<TopicResponseVo>
-     * @throws ReceiptStatusException  ReceiptStatusException
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> clearAutoRenewAccountId(@NonNull String topicId,
                                                                                 @NonNull String adminKey)
@@ -350,21 +221,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return makeTransactionResponse(receipt, new TopicResponseVo(transaction.getTopicMemo()));
     }
 
-    /**
-     * delete Topic
-     * <p>토픽 삭제, 삭제 후 메시지 수신 불가하며 submitMessage 호출 실패, 토픽이 삭제된 후에도 미러 노드를 통해 이전 메시지 엑세스는 가능</p>
-     * <ul>
-     *      <li>토픽 생성 시 adminKey가 설정된 경우 토픽을 성공적으로 삭제하려면 adminKey에 서명해야 합니다.</li>
-     *      <li>토픽 생성 시 adminKey가 설정되지 않은 경우 토픽을 삭제할 수 없으며 UNAUTHHORIZED 오류가 발생합니다.</li>
-     * </ul>
-     *
-     * @param topicId  topicId
-     * @param adminKey 토픽 생성 시 설정한 adminKey
-     * @return
-     * @throws ReceiptStatusException
-     * @throws PrecheckStatusException
-     * @throws TimeoutException
-     */
+
     @Override
     public HederaTransactionResponseVo<TopicResponseVo> deleteTopic(@NonNull String topicId,
                                                                     @NonNull String adminKey)
@@ -393,19 +250,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return makeTransactionResponse(receipt, new TopicResponseVo(topicId));
     }
 
-    /**
-     * submit Message
-     * <p>
-     *
-     * @param topicId    topicId
-     * @param message    message - 최대 크기 1024byte(1kb)
-     * @param chunkSize  메시지에 대한 개별 청크의 최대 크기 - default 1024
-     * @param maxChuncks 메시지 분할 할 수 있는 최대 청크 수 - 기본 값 20
-     * @return HederaTransactionResponseVo<MessageResponseVo>
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     * @throws ReceiptStatusException  ReceiptStatusException
-     */
+
     @Override
     public HederaTransactionResponseVo<MessageResponseVo> submitMessage(@NonNull String topicId,
                                                                         @NonNull String message,
@@ -442,14 +287,7 @@ public class ConsensusHelperV1 extends AbstractHederaHelper implements Consensus
         return makeTransactionResponse(receipt, new MessageResponseVo(topicId, message, receipt.topicSequenceNumber));
     }
 
-    /**
-     * topic 정보 조회
-     *
-     * @param topicId 토픽 아이디
-     * @return TopicInfo
-     * @throws PrecheckStatusException PrecheckStatusException
-     * @throws TimeoutException        TimeoutException
-     */
+
     @Override
     public TopicInfo getTopicInfo(String topicId) throws PrecheckStatusException, TimeoutException {
         //Create the account info query
